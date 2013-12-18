df8f1a450120e40ac4e204775eef4bb0a7da3534
noah pryor <noah@noahpryor.com>
2013-12-18 02:33:39 -0500

    video fixed working without reload

--- client/videos/home_page.js
+++ client/videos/home_page.js
@@ -1,11 +1,32 @@
-  Template.homePage.helpers(
-  { 
+if (Meteor.isClient) {
+   Meteor.startup(function() {
+    Session.setDefault('videoOffset', _.random(300))
+  //  Videos.findOne({},{skip: Session.get('videoOffset')}).fetch().id
+  })
+  Template.homePage.helpers({
+  
     video: function() {
-      var randomVidOffset = _.random(0,665)
-      console.log(randomVidOffset)
-      return Videos.findOne({},{skip: randomVidOffset})
-      }
-})
+      var offset = Session.get('videoOffset')
+      return Videos.findOne({},{skip: offset})
+    }
+  }
+  );
+  Template.homePage.rendered = function() {
+   var video_id = Videos.findOne({},{skip: Session.get('videoOffset')}).url
+   // debugger
+    var video = Popcorn.youtube('#youtube-video', 'http://www.youtube.com/embed/'+video_id);
+    video.play()
+  }
+  Template.homePage.events({
+    'click #new-video': function(e) {
+      console.log(e)
+      Session.set('videoOffset', _.random(300))
+      Deps.flush()
+    }
+  });
+
+}
+
 
 /*
 
